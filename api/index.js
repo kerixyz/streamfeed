511b98a0a5013b669510483372ca2bf313ec5ae4
Keri <kmallari15@gmail.com>
2024-10-19 17:27:49 -0700

    added path import

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const OpenAI = require('openai');
 require('dotenv').config();
+const path = require('path');
 const bodyParser = require('body-parser');
 const pool = require('./db'); // Import database connection
 

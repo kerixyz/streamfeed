4225b5d2a03a54c46746740514f844eb33f4f99e
Keri <kmallari15@gmail.com>
2024-12-04 16:29:16 -0800

    updated footer in chat page

--- frontend/src/pages/Chat.js
+++ frontend/src/pages/Chat.js
@@ -199,7 +199,9 @@ const Chat = () => {
           </div>
           {/* Footer Section */}
           <footer className="py-6 px-6 bg-white text-gray-600">
-            <p>This is an ongoing research project at the University of Washington. By clicking any of the links above, you acknowledge that your feedback may be used for ongoing research to improve the StreamFeed platform. We collect and analyze feedback to better understand how streamers engage with their audiences. Your participation is voluntary, and all data will be anonymized to ensure your privacy.</p>
+            <p>This is an ongoing research project at the University of Washington. Please note that this tool is a prototype so please excuse all the delays and potential errors. If you are also interested in sharing your experience with this tool as a viewer, please reach out to @Kehree on Discord. </p>
+            
+            <p>We collect and analyze feedback to better understand how streamers engage with their audiences. Your participation is voluntary, and all data will be anonymized to ensure your privacy.</p>
             <br/>
             <p>For questions, comments, or feedback, please contact: kmallari[at]uw[dot]edu</p>
           </footer>

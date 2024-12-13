228300deacbd5f3d84fe7ed303a5ea668645f191
kerixyz <desktop@Hecate>
2024-12-13 11:31:17 -0800

    uncommented results

--- frontend/src/components/DashboardView.js
+++ frontend/src/components/DashboardView.js
@@ -119,7 +119,7 @@ const DashboardView = ({ streamer }) => {
             </div>
           ) : (
             <>
-                {/* <section className="w-full max-w-5xl mb-6">
+                <section className="w-full max-w-5xl mb-6">
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                         <div className="p-4 rounded-lg shadow-md border-l-4" style={{ borderColor: '#56e8ad' }}>
                         <h4 className="text-xl font-semibold mb-1">Why Your Viewers Watch You</h4>
@@ -164,7 +164,7 @@ const DashboardView = ({ streamer }) => {
                         </div>
                         ))}
                     </div>
-                </section> */}
+                </section>
             </>
           )}
         </section>
